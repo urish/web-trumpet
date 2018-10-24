db64bcf5b0c1fbb39a691f72db565090428ac70c
Uri Shaked <uri@urishaked.com>
2018-10-25 00:06:29 +0300

    refactor: simplify code

--- index.js
+++ index.js
@@ -3,24 +3,24 @@
 
   const context = new AudioContext();
 
-  function splitNotes(buffer, sampleLength, sampleCount, offset = 0) {
+  function splitNotes(buffer, noteLength, noteCount, offset = 0) {
     const result = [];
     const channels = buffer.numberOfChannels;
-    const sampleFrames = Math.round(sampleLength * buffer.sampleRate);
-    const offsetFrames = Math.round(offset * buffer.sampleRate);
-    const tempBuf = new Float32Array(sampleFrames);
+    const samplesPerNote = Math.round(noteLength * buffer.sampleRate);
+    const offsetSamples = Math.round(offset * buffer.sampleRate);
+    const tempBuf = new Float32Array(samplesPerNote);
 
-    for (let i = 0; i < sampleCount; i++) {
+    for (let i = 0; i < noteCount; i++) {
       result[i] = context.createBuffer(
         channels,
-        buffer.numberOfChannels * sampleFrames,
+        buffer.numberOfChannels * samplesPerNote,
         buffer.sampleRate
       );
       for (let channel = 0; channel < channels; channel++) {
         buffer.copyFromChannel(
           tempBuf,
           channel,
-          i * sampleFrames + offsetFrames
+          i * samplesPerNote + offsetSamples
         );
         result[i].copyToChannel(tempBuf, channel, 0);
       }
@@ -31,36 +31,35 @@
 
   const response = await window.fetch(AUDIO_FILE);
   const arrayBuffer = await response.arrayBuffer();
-  const audioBuffer = await context.decodeAudioData(arrayBuffer);
-  const trumpetBuffer = audioBuffer;
+  const trumpetBuffer = await context.decodeAudioData(arrayBuffer);
   const notes = splitNotes(trumpetBuffer, 4, 43, 0);
 
   function play(audioBuffer) {
-    const source = context.createBufferSource();
+    const sourceNode = context.createBufferSource();
     const gainNode = context.createGain();
-    source.buffer = audioBuffer;
-    source.connect(gainNode);
+    sourceNode.buffer = audioBuffer;
+    sourceNode.connect(gainNode);
     gainNode.connect(context.destination);
-    source.start();
+    sourceNode.start();
     return gainNode;
   }
 
-  async function playNote(note, length) {
+  async function delay(seconds) {
+    await new Promise(resolve => setTimeout(resolve, seconds * 1000));
+  }
+
+  async function playNote(note, seconds) {
     const octave = parseInt(note[note.length - 1]) + 1;
     const noteIdx = 'C,C#,D,D#,E,F,F#,G,G#,A,A#,B'
       .split(',')
       .indexOf(note.substr(0, note.length - 1));
     const idx = octave * 12 + noteIdx;
     const gainNode = play(notes[idx - 54]);
-    await new Promise(resolve => setTimeout(resolve, length * 1000));
+    await delay(seconds);
     gainNode.gain.exponentialRampToValueAtTime(
       0.00001,
       context.currentTime + 0.1
     );
-  }
-
-  async function delay(length) {
-    await new Promise(resolve => setTimeout(resolve, length * 1000));
   }
 
   async function melody() {
